5c03eb20082448f7e9659cfc0e683817a7da9425
aakash-01-1996 <aakashambodkar99@gmail.com>
2024-12-03 21:31:28 -0700

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Aakash Ambodkar on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
